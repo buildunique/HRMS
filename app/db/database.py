f62b2f9e2930cd5af70855b529130057796c102c
buildunique <113715371+buildunique@users.noreply.github.com>
2026-02-21 16:11:32 +0530

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./hrms.db"
+# Vercel has read-only filesystem except /tmp
+DB_PATH = '/tmp/hrms.db'
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
